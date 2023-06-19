9d0fe4a140a187d089e000f7905c45d6f53474f4
CS-Edwards <candaces.edwards@gmail.com>
2023-06-19 13:36:54 -1000

    completed rand tutorial

--- 2_inf_plant/sketch.js
+++ 2_inf_plant/sketch.js
@@ -5,17 +5,21 @@
 
 
 /**
-Example 7: Fractal plant
-See also: Barnsley fern
+Example : Barney Codes 
 variables : X F
 constants : + − [ ]
 start  : X
-rules  : (X → F+[[X]-X]-F[-FX]+X), (F → FF)
+rules  : (X → F[+X][-X]FX), (F → FF)
+
+
+updated rules: (X → F[+X][-X]FX, X → F[-X]FX, X → F[+X]FX)
+
+
+
 angle  : 25°
 Here, F means "draw forward", − means "turn right 25°", and + means "turn left 25°". X does not correspond to any drawing action and is used to control the 
 evolution of the curve. The square bracket "[" corresponds to saving the current values for position and angle, which are restored when the corresponding "]"
 is executed.
-
  */
 
 /** Draw forward in p5.js:
@@ -23,14 +27,29 @@ is executed.
  * meaning we move the origin of the line to the end of the next line and begin drawing from that point.
  */
 
+
 //SYSTEM RULES
 let rules = {
-    "X": "F+[[X]-X]-F[-FX]+X",
-    "F":"FF"
-}
+    "X": [{rule: "F[+X][-X]FX", prob: 0.5},
+          {rule: "F[-X]FX", prob: 0.05},
+          {rule: "F[+X]FX", prob: 0.05},
+          {rule: "F[++X][-X]FX", prob: 0.1}, //extra rotations from side branches
+          {rule: "F[+X][--X]FX", prob: 0.1},   //extra rotations from side 
+          
 
-let len = 3;
-let ang = 25;
+          //Additional Fruiting Rules A and B
+          {rule: "F[+X][-X]FA", prob: 0.1}, 
+          {rule: "F[+X][-X]FB", prob: 0.1},
+        ],
+    "F":[{rule: "FF", prob: 0.85},
+         {rule: "FFF", prob: 0.05}, //extra growth
+         {rule: "F", prob: 0.1}, //stunting
+        ]
+}
+const len = 4;
+const ang = 25;
+const numGens = 6;
+
 let word = "X";
 
 //DRAW RULES
@@ -43,14 +62,29 @@ let drawRules;
 
 
 function setup(){
-    createCanvas(400,400);
+    createCanvas(600,600);
     noLoop();
 
+    strokeWeight(2);
+
     drawRules ={
+        //fruiting rules
+        "A":()=>{
+            noStroke();
+            fill("#E5CEDC");
+            circle(0,0,len*2);
+        },
+
+        "B": ()=>{
+            noStroke();
+            fill("#FCA17D");
+            circle(0,0,len*2);
+        },
+
         //draw forward
         "F": ()=>{
-            //color leaves brown
-            stroke(150,100,0);
+            //color green
+            stroke("#9ea93f");
             //-len means draw up
             line(0,0,0,-len);
             //move to end of drawn line
@@ -68,13 +102,7 @@ function setup(){
         //push information into save state
         "[": push,
         // meas we've reached the end of the branch
-        "]": ()=>{
-            //noStroke();
-            fill(0,255,0);
-            //leaf shape
-            ellipse(0,0,2,5);
-            pop();
-        }
+        "]": pop,
     }
 }
 
@@ -95,7 +123,7 @@ function draw(){
 
     //start drawing at bottom of canvas, a quarter in to the canvas and rotate
     translate(width/4,height);
-    rotate(PI/180 *ang);
+    //rotate(PI/180 *ang);
 
     for(let i =0;i<word.length;i++){
         let c = word[i];
@@ -114,6 +142,23 @@ function mouseReleased(){
 
 }
 
+function chooseOne(ruleSet){
+    //generate number between 0 and 1
+    let n = random();
+    let t = 0;
+
+    //loop through all rules to sum probability (sum of all probs = 1)
+    for(let i=0; i< ruleSet.length; i++){
+        t += ruleSet[i].prob;
+        //until total probability has surpassed random n
+        if(t>n){
+            return ruleSet[i].rule;
+        }
+    }
+    return "";
+
+}
+
 //generate() creates the sequence for the fractal structure
 // returns string of next rule
 function generate(){
@@ -121,10 +166,19 @@ function generate(){
     //loop over every character in every word
     for(let i=0; i<word.length;i++){
         let c = word[i];
-        //if c is in our rules, add rules sequnce to next 
-        //else add character to next gen (symbols carry forward)
+
+        //Array of different rules that a single character can map to
+        // and a series of probabilites so we can weight how the L- System will choose
+        // the amoung the different rules.
+        
         if(c in rules){
-            next+=rules[c];
+            let rule = rules[c];
+            //If the rule is an array, chooseOne() choses one of the options from the rule set
+            if(Array.isArray(rule)){
+                next += chooseOne (rule);
+            }else{
+                next += rules[c];
+            }
         }else{
             next+=c;
         }
